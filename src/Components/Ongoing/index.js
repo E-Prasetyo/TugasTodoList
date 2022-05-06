9ad23de1179d2d426c38df3912044246d5cc444d
E Prasetyo <67994910+E-Prasetyo@users.noreply.github.com>
2022-05-06 20:20:23 +0700

    Update simple

--- src/Components/Ongoing/index.js
+++ src/Components/Ongoing/index.js
@@ -22,6 +22,40 @@ const Ongoing = ({myData, setMyData}) => {
    const handleDeleteTodoList = (id) => {
        deleteTodoList(setMyData, id);
    }
+
+   const listData = myData?.filter((item)=> item?.ongoing === true)?.map((item) => {
+        return(
+            <div className='bg-primary px-5 py-2 my-3 text-start rounded-pill' key={item.id} >
+                <div className='d-flex flex-row justify-content-between align-items-center'>
+                    <div className='flex-col align-items-center'>
+                        <div className='text-white'>{item.title}</div>
+                        <div className='text-white'>{item.dateStart}</div>
+                    </div>
+                    <div className='d-flex flex-row gap-4 py-2'>
+                        <button 
+                            className='btn btn-warning rounded-pill btn-shadow'
+                            onClick={handleOpen.bind(null, item.id)}
+                        >
+                            Edit
+                        </button>
+                        <button 
+                            className='btn btn-success text-black rounded-pill btn-shadow'
+                            onClick={handleDoneTodoList.bind(null, item.id)}
+                        >
+                            Done
+                        </button>
+                        <button 
+                            className='btn btn-danger text-black rounded-pill btn-shadow'
+                            onClick={handleDeleteTodoList.bind(null, item.id)}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+        )
+    })
     
   return (
     <>
@@ -38,39 +72,7 @@ const Ongoing = ({myData, setMyData}) => {
                 </div>
                     <div className='container w-100 d-flex flex-column py-4 gap-4'>
                         <div className='container bg-create-title content-rounded p-3 w-75'>
-                            {myData?.filter((item)=> item?.ongoing === true)?.map((item) => {
-                                return(
-                                    <div className='bg-primary px-5 py-2 my-3 text-start rounded-pill' key={item.id} >
-                                        <div className='d-flex flex-row justify-content-between align-items-center'>
-                                            <div className='flex-col align-items-center'>
-                                                <div className='text-white'>{item.title}</div>
-                                                <div className='text-white'>{item.dateStart}</div>
-                                            </div>
-                                            <div className='d-flex flex-row gap-4 py-2'>
-                                                <button 
-                                                 className='btn btn-warning rounded-pill btn-shadow'
-                                                 onClick={handleOpen.bind(null, item.id)}
-                                                >
-                                                    Edit
-                                                </button>
-                                                <button 
-                                                    className='btn btn-success text-black rounded-pill btn-shadow'
-                                                    onClick={handleDoneTodoList.bind(null, item.id)}
-                                                >
-                                                    Done
-                                                </button>
-                                                <button 
-                                                    className='btn btn-danger text-black rounded-pill btn-shadow'
-                                                    onClick={handleDeleteTodoList.bind(null, item.id)}
-                                                >
-                                                    Delete
-                                                </button>
-                                            </div>
-                                        </div>
-                                        
-                                    </div>
-                                )
-                            })}
+                            {listData}
                         </div>
                      
                     </div>
